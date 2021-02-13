92017a258d715f8f9578c384db6ed332aea17fbe
xvenge00 <jawe1222@gmail.com>
2021-02-13 17:19:19 +0100

    base prb response in fuzzer

--- src/debug.h
+++ src/debug.h
@@ -1,10 +1,22 @@
 #ifndef CPP_DEBUG_H
 #define CPP_DEBUG_H
 
+#include <iomanip>
 
 void print_mac(const uint8_t *mac) {
     fprintf(stderr, "%02x:%02x:%02x:%02x:%02x:%02x\n",
            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+}
+
+void print_bytes(std::ostream& out, const unsigned char *data, size_t dataLen, bool format = true) {
+    out << std::setfill('0');
+    for(size_t i = 0; i < dataLen; ++i) {
+        out << std::hex << std::setw(2) << (int)data[i];
+        if (format) {
+            out << (((i + 1) % 16 == 0) ? "\n" : " ");
+        }
+    }
+    out << std::endl;
 }
 
 #endif //CPP_DEBUG_H
