7631252803f6734a0cf9aec24a4d30a7d38edd5b
xvenge00 <jawe1222@gmail.com>
2021-04-12 12:56:14 +0200

    add string and uint16 prepared fuzz lists

---
+++ src/fuzzer/utils/int.h
@@ -0,0 +1,14 @@
+#ifndef CPP_INT_H
+#define CPP_INT_H
+
+#include <cinttypes>
+#include <vector>
+
+namespace primitives {
+
+extern std::vector<std::uint16_t> fuzz_uint16;
+extern std::vector<std::uint16_t> fuzz_uint16_bigger_complement;
+
+}
+
+#endif //CPP_INT_H
