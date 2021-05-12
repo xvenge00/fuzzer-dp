c03af74f28b35f823cf1fcaf96730190cd5005ae
xvenge00 <jawe1222@gmail.com>
2021-05-12 16:21:51 +0200

    fix channel config loading

--- src/config/config_loader.h
+++ src/config/config_loader.h
@@ -131,7 +131,7 @@ Config load_config(const std::filesystem::path &config_file) {
         .random_seed = config_node["random_seed"].as<unsigned>(),
         .src_mac = parse_mac(config_node["src_mac"].as<std::string>()),
         .test_device_mac = parse_mac(config_node["test_device_mac"].as<std::string>()),
-        .channel = config_node["channel"].as<std::uint8_t>(),
+        .channel = (std::uint8_t) config_node["channel"].as<unsigned>(),    // as std::uint8 doesn't work
         .fuzzer_type = parse_fuzzer_type(config_node["fuzzer_type"].as<std::string>()),
         .fuzz_random = config_node["fuzz_random"].as<unsigned>(),
         .set_up = parse_set_up(config_node["set_up"].as<std::string>()),
